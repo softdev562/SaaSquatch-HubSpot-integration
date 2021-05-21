04f3f336bae225099d0310cc7489400c6f0bbdc7
softdev562 <smithdustin604@gmail.com>
2021-05-21 15:09:47 -0700

    Able to get a list of contacts.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-import express from 'express'
-import 
+import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
 
 // constants
-const port = process.env.PORT
+dotenv.config({path: __dirname+'/index.env'});
+const port = process.env.PORT;
+const hapikey = process.env.HAPIKEY;
 
 // initialize application
 const app = express();
-app.get('/', (_, res) => {
+const hs = 'hubspot';
+const sq = 'saasquatch';
+app.get('/', (req, res) => {
 	res.status(200).send('Hello world!')
 })
 
-app.listen(port, () => console.log(`running express application on localhost:${port}`))
+// /hubspot/contacts?limit=n&archived=b
+// n is the maximum number of contacts.
+// b indicates whether to include archived contacts
+app.get(`/${hs}/contacts`, (req, res) => {
+	axios.get(`https://api.hubapi.com/crm/v3/objects/contacts?limit=${req.query.limit}&archived=${req.query.archived}&hapikey=${hapikey}`)
+		.then(axiosRes => {
+			res.send(axiosRes.data);
+		})
+		.catch (err => {
+			res.send(err);
+		});
+});
 
+app.listen(port, () => console.log(`running express application on localhost:${port}`))
