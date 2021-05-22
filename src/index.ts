4616db29b1c3e55092797d8d81b49c9501ec0f62
softdev562 <smithdustin604@gmail.com>
2021-05-21 17:23:54 -0700

    Updated axios syntax to make is easier to read. Can now add contacts using the /hubspot/addcontact endpoint.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
+import bodyParser from 'express'
 
 // constants
 dotenv.config({path: __dirname+'/index.env'});
@@ -11,6 +12,9 @@ const hapikey = process.env.HAPIKEY;
 const app = express();
 const hs = 'hubspot';
 const sq = 'saasquatch';
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
 	res.status(200).send('Hello world!')
 })
@@ -19,13 +23,28 @@ app.get('/', (req, res) => {
 // n is the maximum number of contacts.
 // b indicates whether to include archived contacts
 app.get(`/${hs}/contacts`, (req, res) => {
-	axios.get(`https://api.hubapi.com/crm/v3/objects/contacts?limit=${req.query.limit}&archived=${req.query.archived}&hapikey=${hapikey}`)
-		.then(axiosRes => {
-			res.send(axiosRes.data);
-		})
-		.catch (err => {
-			res.send(err);
-		});
+	axios({
+		method: 'get',
+		url: `https://api.hubapi.com/crm/v3/objects/contacts?limit=${req.query.limit}&archived=${req.query.archived}&hapikey=${hapikey}`
+	}).then(axiosRes => {
+		res.send(axiosRes.data);
+	}).catch(err => {
+		res.send(err);
+	});
+});
+
+// /hubspot/addcontact
+// Adds a contact to the hubspot account.
+app.post(`/${hs}/addcontact`, (req, res) => {
+	axios({
+		method: 'post',
+		url:`https://api.hubapi.com/crm/v3/objects/contacts?hapikey=${hapikey}`,
+		data: req.body
+	}).then(axiosRes =>{
+		res.send(axiosRes.data);	
+	}).catch(err => {
+		res.send(err);
+	});
 });
 
 app.listen(port, () => console.log(`running express application on localhost:${port}`))
