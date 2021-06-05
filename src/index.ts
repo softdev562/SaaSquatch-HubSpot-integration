08191222356601ee68ab25d8a7325e0bd6d534d4
softdev562 <smithdustin604@gmail.com>
2021-06-05 00:02:26 -0700

    Created unified project directory for frontend and backend.

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import path from 'path'
+import chalk from 'chalk'
+import routes from './routes'
+
+// constants
+const PORT = process.env.PORT || 8000
+const {
+	HUBSPOT_API_KEY: HAPIKEY,
+	SAASQUATCH_API_KEY: SAPIKEY,
+	SAASQUATCH_TENANT_ALIAS: STENANTALIAS
+} = process.env
+
+// configure
+const app = express()
+
+// dynamic routes
+app.use(routes)
+
+// static routes
+app.use(express.static(path.join(__dirname, '../public')))
+app.get('/', (_, res) => {
+	res.sendFile(path.join(__dirname, '../public', 'index.html'))
+})
+app.get('*', (_, res) => {
+	res.redirect('/')
+})
+
+// launch
+app.listen(PORT, () => console.log(
+	chalk.bold('SaaSquatch-HubSpot') +
+	' integration listening on port ' +
+	chalk.blue.bold(PORT)
+))
