06d2216395822a0e043bf5271caef570b8124b28
softdev562 <smithdustin604@gmail.com>
2021-06-06 22:28:46 -0700

    Installed firebase requieremnts. It should be possible to make calls now

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import firebase from 'firebase/app'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbxaEk-eim7Y7jR8Cuv1mt_qk_v8Jg-O8",
+  authDomain: "integration-database-5dfe7.firebaseapp.com",
+  databaseURL: "https://integration-database-5dfe7-default-rtdb.firebaseio.com",
+  projectId: "integration-database-5dfe7",
+  storageBucket: "integration-database-5dfe7.appspot.com",
+  messagingSenderId: "1001945617105",
+  appId: "1:1001945617105:web:b32f8885a9d9e5224febf0",
+  measurementId: "G-VDBF663K1R"
+};
 
 ReactDOM.render(
 	<React.StrictMode>
