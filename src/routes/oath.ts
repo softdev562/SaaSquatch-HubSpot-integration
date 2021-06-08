a4224a01c24b1cdf6d909d248936f85eda11cd1d
softdev562 <smithdustin604@gmail.com>
2021-06-08 11:16:02 -0700

    get auth0 JWT from saasquatch

--- src/routes/oath.ts
+++ src/routes/oath.ts
@@ -1,18 +1,19 @@
-import { Router } from 'express'
+import { Router } from 'express';
 require('dotenv').config();
 import axios from 'axios';
 import querystring from 'querystring';
 
-
-
-const router = Router()
+const router = Router();
 
 // env constants
+// Hubspot
 const HUBSPOT_CLIENT_ID = process.env.HUBSPOT_CLIENT_ID;
 const HUBSPOT_CLIENT_SECRET = process.env.HUBSPOT_CLIENT_SECRET;
 const HUBSPOT_REDIRECT_URI = process.env.HUBSPOT_REDIRECT_URI;
 const HUBSPOT_AUTH_URL = `https://app.hubspot.com/oauth/authorize?client_id=${HUBSPOT_CLIENT_ID}&redirect_uri=${HUBSPOT_REDIRECT_URI}&scope=contacts`;
-
+// Saasquatch
+const SAASQUATCH_CLIENT_ID = process.env.SAASQUATCH_CLIENT_ID;
+const SAASQUATCH_CLIENT_SECRET = process.env.SAASQUATCH_CLIENT_SECRET;
 
 // Temp token store, 
 // TODO: move to Firebase DB?
@@ -22,6 +23,39 @@ const isAuthorized = (userId: string) =>{
     return tokenStore[userId] ? true : false;
 };
 
+// Gets a new access token from saasquatch
+// return: access token object, or error object.
+// Access token object: {"access_token", "expires_in", "token_type"}
+// Error object: {"error", "error_description"}
+const getSaasquatchToken = async () =>  {
+	// Return early if missing some env variables.
+	if (!SAASQUATCH_CLIENT_ID || !SAASQUATCH_CLIENT_SECRET) {
+		return {"error": "Client id/secret error", "error_description": "Client id or secret not provided correctly."};
+	}
+
+	const url: string = "https://squatch-dev.auth0.com/oauth/token";
+	const grant_type: string = "client_credentials";
+	const audience: string = "https://staging.referralsaasquatch.com";
+
+	const body = {
+		"grant_type": grant_type,
+		"client_id": SAASQUATCH_CLIENT_ID,
+		"client_secret": SAASQUATCH_CLIENT_SECRET,
+		"audience": audience
+	};
+
+	try {
+		const resp = await axios.post(url, body);
+		if (resp.status != 200) {
+			console.log(resp.data["error"]);
+			return resp.data;
+		}
+		return resp.data;
+	} catch(e) {
+		console.log(e);
+		return {"error": e, "error_description": e};
+	}
+};
 
 // Start HubSpot OAuth flow
 // 1. Send user to authorization page
@@ -32,7 +66,7 @@ router.get('/hubspot', async (req, res) => {
         const headers = {
             Authorization: `Bearer ${accessToken}`,
             'Content-Type': 'application/json'
-        }
+        };
         const contacts = 'https://api.hubapi.com/contacts/v1/lists/all/contacts/recent';
         try{
             const resp = await axios.get(contacts, {headers});
@@ -46,10 +80,10 @@ router.get('/hubspot', async (req, res) => {
         if(HUBSPOT_AUTH_URL){
             res.redirect(HUBSPOT_AUTH_URL);
         }else{
-            console.error("env AUTH_URL is undefined.")
+            console.error("env AUTH_URL is undefined.");
         }
     }
-})
+});
 
 // 2. Get temporary authorization code from OAuth server
 // 3. Combine temporary auth code with app credentials and send back to OAuth server
@@ -76,9 +110,20 @@ router.get('/oauth-callback', async (req, res) => {
     }
     // Log error if no code received
     else{
-        console.error("HubSpot OAuth callback did not receive temp access code.")
+        console.error("HubSpot OAuth callback did not receive temp access code.");
     }
-})
+});
+
+// Test route, delete later
+router.get("/saasquatch_token", async (req, res) => {
+	try {
+		const token = await getSaasquatchToken();
+		res.send(token)
+	} catch(e) {
+		console.log(e);
+		res.send(e)
+	}
+});
 
 
 export default router
