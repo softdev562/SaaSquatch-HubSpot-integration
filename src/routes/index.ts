2b7a7adf5f8f70ea81a7cd0a81c3f8f51d389904
softdev562 <smithdustin604@gmail.com>
2021-06-09 15:17:07 -0700

    Update index.ts
    adding functionality to get participants list from saasquatch and post new users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
 const axios = require("axios");
+const http = require("http");
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-if (!process.env.HAPIKEY) {
+if (!process.env.HAPIKEY || !process.env.SAPIKEY || !process.env.STENANTALIAS) {
     throw new Error('Missing HAPIKEY environment variable.')
 }
+
 const HAPIKEY = process.env.HAPIKEY;
+const SAPIKEY = process.env.SAPIKEY;
+const STENANTALIAS = process.env.STENANTALIAS;
 
 const router = Router()
 router.get('/api/', (_, res) => {
@@ -40,6 +44,34 @@ router.get('/contacts', async (req, res) => {
     const contact = await getContacts();
     res.json(contact);
     console.log('got contacts');
+    res.end();
+});
+
+const getParticipants = async () => {
+    try {
+        console.log('=== attempting to get all SaaSquatch participants api key is ===' + SAPIKEY);
+        const allParticipants = 'https://staging.referralsaasquatch.com/api/v1/' +STENANTALIAS+ '/users';
+        console.log(allParticipants);
+        const response = await axios.get(allParticipants, { headers: { 'User-Agent': SAPIKEY }  } );
+        console.log(response);
+        const data = response.data;
+        console.log(data);
+        return data;
+    } catch (e) {
+        console.error('  > Unable to retrieve participants');
+        console.log(e);
+        return JSON.parse(e);
+    }
+	
+
+}
+
+router.get('/participants', async (req, res) => {
+
+    const Participants = await getParticipants();
+    console.log('res:' + res)
+    res.json(Participants);
+    console.log('got Participants');
     res.end();
 });
 export default router
