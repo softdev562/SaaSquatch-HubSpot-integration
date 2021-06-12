c0c5d9f09e1eb8096f49f480f11ae96b10db4c14
softdev562 <smithdustin604@gmail.com>
2021-06-11 22:50:28 -0700

    Added Hubspot webhooks validation check

--- src/routes/webhooks.ts
+++ src/routes/webhooks.ts
@@ -1,9 +1,10 @@
+require('dotenv').config();
 import { Router } from 'express';
 import * as jwt from "jsonwebtoken";
 import jwksRsa = require("jwks-rsa");
 import { Base64 } from "js-base64";
-import bodyParser = require('body-parser');
-import { type } from 'os';
+import crypto from "crypto";
+import e = require('express');
 
 /**
  * Handles Webhooks from SaaSquatch and Hubspot
@@ -11,16 +12,19 @@ import { type } from 'os';
 
 const router = Router();
 
-// Endpoint for listening for webhooks from SaaSquatch
+const HUBSPOT_CLIENT_SECRET = process.env.HUBSPOT_CLIENT_SECRET;
+
+
+// Endpoint for webhooks from SaaSquatch
 router.post("/saasquatch-webhook", async (req, res) => {
-    console.log(req.body);
     const jwsNoPayloadHeader: string | undefined = req.get("X-Hook-JWS-RFC-7797");
     if (jwsNoPayloadHeader){
         validateSaaSquatchWebhook(JSON.stringify(req.body), jwsNoPayloadHeader)
         .then(decoded =>{
+            console.log("Received valid SaaSquatch Webhook.")
             console.log(decoded);
             /**
-             * Here you would process webhook. ie. determine "type" and from there post to 
+             * Here you would process the webhook. ie. determine "type" and from there post to 
              * Hubspot API with update, etc.
              */
             res.status(200).end();
@@ -36,14 +40,33 @@ router.post("/saasquatch-webhook", async (req, res) => {
 });
 
 
-// Placeholder for hubspot webhook validation
+// Endpoint for webhooks from HubSpot
 router.post("/hubspot-webhook", async (req, res) => {
-    res.status(200).send("Not yet implemented.");
+    const signatureVersion = req.get("X-HubSpot-Signature-Version");
+    const signature = req.get("X-HubSpot-Signature");
+    if (signatureVersion && signature){
+        const isValid: boolean = validateHubSpotWebhook(signatureVersion, signature, JSON.stringify(req.body));
+        if (isValid){
+            console.log("Received valid HubSpot Webhook.");
+            console.log(req.body);
+            /**
+             * Here you would process the webhook. ie. determine "type" and from there post to 
+             * SaaSquatch API with update, etc.
+             */
+            res.status(200).end();
+        }
+        else{
+            console.warn("Received invalid HubSpot Webhook.")
+            res.status(401).end();
+        }
+    }else{
+        res.status(401).end();
+    }
 });
 
 
 /**
- * Verifying a Webhook Payload: 
+ * Verifying a SaaSquatch Webhook Payload: 
  * 
  * 1. Ensure the X-Hook-JWS-RFC-7797 header exists. If it doesn't exist, this request didn't come from SaaSquatch.
  * 2. Look up the SaaSquatch JWKS at http://app.referralsaasquatch.com/.well-known/jwks.json. This contains the 
@@ -99,5 +122,55 @@ export function validateSaaSquatchWebhook(
     const token = jwsNoPayloadHeader.replace("..", "." + webhookBodyBase64 + ".");
     return validateWithSaaSquatchJwks(token);
 }
+
+
+
+
+/**
+ * Verifying a HubSpot Webhook Payload: 
+ * 
+ * 1. Check the X-HubSpot-Signature and X-HubSpot-Signature-Version headers exists. If these don't, this request 
+ *    didn't come from HubSpot.
+ * 2. Check the X-HubSpot-Signature-Version for the version.
+ * 3. To validate the v1 request signature:
+ *      a) Create a string concatinating client_secret and request body
+ *      b) Create a SHA-256 hash of the string
+ *      c) Compare hash to X-HubSpot-Signature
+ * 4. To validate the v2 request signature:
+ *      a) Create a string concatinating the Client secret + http method + URI + request body
+ *      b) Create a SHA-256 hash of the string
+ *      c) Compare hash to X-HubSpot-Signature
+ * 
+ * REF: https://developers.hubspot.com/docs/api/webhooks/validating-requests
+ * 
+ */
+
+/**
+ * Validate a webhook coming from HubSpot.
+ *
+ * @param webhookBody The raw text of the webhook body.
+ * @param signatureVersion The value of the X-HubSpot-Signature-Version header.
+ * @param signature The value of the X-HubSpot-Signature header
+ */
+ export function validateHubSpotWebhook(signatureVersion: string, signature: string, webhookBody?: string) {
+    if(!HUBSPOT_CLIENT_SECRET){
+        console.error("Missing HUBSPOT_CLIENT_SECRET in env variables.");
+        return false;
+    }
+    var source_string: string;
+    if(signatureVersion == 'v1'){
+        source_string = webhookBody? HUBSPOT_CLIENT_SECRET + webhookBody : HUBSPOT_CLIENT_SECRET;
+    }else if(signatureVersion == 'v2'){
+        const uri = encodeURI("http://localhost:8000/hubspot-webhook");
+        source_string = webhookBody? HUBSPOT_CLIENT_SECRET + "POST" + uri + webhookBody : HUBSPOT_CLIENT_SECRET + "POST" + uri
+    }
+    else{
+        console.error("Invalid or missing X-HubSpot-Signature-Version header.");
+        return false;
+    }
+    const hash = crypto.createHash('sha256').update(source_string).digest('hex');
+    return hash == signature;
+}
+
 
 export default router;
