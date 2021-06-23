05b6b66acc8f823c4e40dc6a047a500cab45bb10
softdev562 <smithdustin604@gmail.com>
2021-06-22 21:53:09 -0600

    removed white space

--- src/routes/oath.ts
+++ src/routes/oath.ts
@@ -147,14 +147,10 @@ router.get("/hubspot_refresh_token", async (req, res) => {
 
 export const HubApiCall:any = async function (myapifunc:Function,refresh_token:string)
 {
-
 	// first try to see if the api call goes through if it does then send response back
 	try
 	{
-
 		let result = await myapifunc()
-
-
 		// api call went through everything is fine
 		return result;
 	}
@@ -162,13 +158,10 @@ export const HubApiCall:any = async function (myapifunc:Function,refresh_token:s
 	{
 		try
 		{
-
 			//error in the api call get a new access token
 			let result = await getHubspotAccessToken(refresh_token);
-
 			if(result.status ==400)//result.response.status == 400)
 			{
-
 				if(process.env.NODE_ENV == 'test')
 				{
 					// return error result for the testing purpose
@@ -178,34 +171,24 @@ export const HubApiCall:any = async function (myapifunc:Function,refresh_token:s
 				{
 				//below is equivalent to rejecting promise return Promise.reject(400 /*or Error*/ );
 					return JSON.parse(result);
-
 				}
-
 			}
 			else
 				// there was no error so we can store the result.
 			{
-
 				// #todo: Update the line below configure with DB
 				tokenStore[current_user] = {"access_token": result};
-
 				return await HubApiCall(myapifunc,refresh_token);
-
 				// over here we probably want to call the ApiCall again with the same arguments
 				// or we want to redirect to the url we were called from
 			}
-
 		}
-
 		catch(e)
 		{
 			//something went wrong?
 			return Promise.reject(400 /*or Error*/ );
-
 		}
 	}
-
-
 }
 
 
