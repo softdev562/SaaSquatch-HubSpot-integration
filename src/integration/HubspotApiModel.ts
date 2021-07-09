b0e50cb6bb55e735d74665babb787fb7647741e0
softdev562 <smithdustin604@gmail.com>
2021-07-09 15:59:18 -0700

    Created sync for referral links from saasquatch to hubspot by creating new property fields in the contacts Object

--- src/integration/HubspotApiModel.ts
+++ src/integration/HubspotApiModel.ts
@@ -63,6 +63,74 @@ export class HubspotApiModel {
             return JSON.parse(e.response.body);
         }
     }
+    /**
+     * 
+     * @param objectType object to create property to check property for 
+     * @param propertyName the name of the property, not the label
+     * @returns true if the property exists, otherwise false
+     */
+    public async objectHasProperty(objectType: string, propertyName: string){
+        const readPropertyURL = 'https://api.hubapi.com/crm/v3/properties/' + objectType + '/' + propertyName; 
+        try {
+         const response = await axios.get(readPropertyURL, {
+             params: {
+                 hapikey: this.HAPIKEY
+             }
+         });
+            if (response.status==200 ){
+                console.log("=== PROPERTY EXIST IN OBJECT TYPE " + objectType + " ===")
+                return true; 
+            } else {
+                console.error("======== WAS NOT ABLE TO READ PROPERTY ========");
+                console.error(response);
+            }
+        } catch (e) {
+            if(e.response.status==404){
+                console.log("=== PROPERTY DOES NOT EXIST IN OBJECT TYPE " + objectType + " ===")
+                return false;
+            } else{
+                console.error("======== WAS NOT ABLE TO MAKE CALL: STATUS CODE: "+ e.response.status+" ========");
+                console.log(e);
+                return JSON.parse(e.response.body);
+            }
+        }
+    }
+
+
+    /**
+     * Create a hubspot property for an object
+     * 
+     * @param objectType object to create property for
+     * @param propertyName The internal property name, which must be used when referencing the property via the (hubspot's) API.
+     * @param propertyLabel A human-readable property label that will be shown in HubSpot.
+     * @param propertyType The data type of the property.
+     * @param propertyFieldType Controls how the property appears in HubSpot.
+     * @param propertyGroupName The name of the property group the property belongs to.
+     * https://developers.hubspot.com/docs/api/crm/properties
+     */
+    public async createObjectProperty(objectType:string, propertyName: string, propertyLabel:string, propertyType:string, propertyFieldType:string, propertyGroupName: string){
+        const contactCreatePropertyURL = 'https://api.hubapi.com/crm/v3/properties/' + objectType;
+        const body = {
+            "name": propertyName,
+            "label": propertyLabel,
+            "type": propertyType,
+            "fieldType": propertyFieldType,
+            "groupName": propertyGroupName,
+            "formField": true
+    
+        };
+        try {
+            await axios.post(contactCreatePropertyURL, body, {
+                params: {
+                    hapikey:this.HAPIKEY
+                }
+            })
+        } catch (e) {
+            console.error("==== WAS NOT ABLE TO POST NEW PROPERTY ===");
+            console.log(e);
+            return JSON.parse(e.response.body);
+        }
+    }
    
 
     /**
